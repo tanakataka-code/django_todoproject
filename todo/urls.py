6a583f8f6d6adb636ad6cb22f21b4352f421fb5d
tanaka takayuki <hawk.snow.1324@icloud.com>
2021-02-21 15:02:17 +0900

    bootstrap適用、base.html作成

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from .views import TodoList, TodoDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', TodoList.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view())
 ]
